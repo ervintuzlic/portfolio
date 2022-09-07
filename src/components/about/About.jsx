5c040a3bf9b2de37ed62ea00375c51eff55278e6
Ervin Tuzlic <ervin@fragnet.net>
2022-09-07 13:21:11 +0200

    ErvinPortfolio2Finished

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,14 +10,19 @@ const About = () => {
         <img src={Avatar} alt="avatar" class="avatar" />
         <div class="aboutSection">
           <h3>Ervin Tuzlic</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident eligendi, voluptas quidem a tempore pariatur omnis aliquid? Nisi doloribus laudantium provident fugiat cumque consectetur accusamus sit dicta error, ratione quam.</p>
+          <p>Software engineer eager to devote his full attention to the world of programming.
+            As someone who studies software engineering, I have experience in ASP.NET MVC, HTML, CSS,
+            JavaScript, and jQuery, and I am quite familiar with the syntax and logic of C++ and C#.</p>
           <br></br>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident eligendi, voluptas quidem a tempore pariatur omnis aliquid? Nisi doloribus laudantium provident fugiat cumque consectetur accusamus sit dicta error, ratione quam.</p>
+          <p>I have a strong passion for programming, but I have also spent a significant part of my life in the
+            design industry, gaining three years of expertise in using tools like Photoshop, Adobe Illustrator,
+            Figma, and others to create magic.</p>
           <br></br>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident eligendi, voluptas quidem a tempore pariatur omnis aliquid? Nisi doloribus laudantium provident fugiat cumque consectetur accusamus sit dicta error, ratione quam.</p>
+          <p>As someone who always likes to learn something new and get to know every corner of IT, I look
+            forward to new collaborations and programming problems that are waiting for me to solve!</p>
         </div>
         <div class="ContactMe2">
-          <a href="#contact"><button type="button" className="SendMessage" style={{cursor:"pointer" }}>Contact me!</button></a>
+          <a href="#contact"><button type="button" className="SendMessage" style={{cursor:"pointer" }}>Download CV</button></a>
         </div>
       </div>
     </section>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './Contact.css';
 import {FaLinkedinIn} from 'react-icons/fa';
 import {FiGithub} from 'react-icons/fi';
@@ -7,16 +8,34 @@ import {AiFillMail} from 'react-icons/ai';
 import {HiLocationMarker} from 'react-icons/hi';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_88zmeuc', 'template_g507759', form.current, 'x2NdWI8CioalFCQv1')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
+
   return (
     <section id='contact'>
       <div id="contactContainer">
         <h3>Contact me!</h3>
         <div id="contactSection">
           <div id="messageContainer">
-            <input type="text" placeholder="Your Name: " />
-            <input placeholder="Your Email Address: " />
-            <textarea placeholder="Your Message:  " />
-            <button>Send Message</button>
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name='name' placeholder="Your Name: " required />
+              <input name='emailAddress' placeholder="Your Email Address: " required />
+              <textarea name='message' placeholder="Your Message:  " required />
+              <button class="sendMessageButton" type="submit">Send Message</button>
+            </form>
           </div>
           <div id="details">
             <div id="contactDetails">
